c24cda524947282f81813a00c9fa0923a366f6aa
ThangVPham <78657808+ThangVPham@users.noreply.github.com>
2021-05-17 12:40:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+(function(){
+    function Start(){
+        let myVariable = 20;
+        console.log("My Number is: " + myVariable);
+    }
+    window.addEventListener("load", Start);
+})();
